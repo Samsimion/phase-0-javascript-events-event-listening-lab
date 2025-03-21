83b0ddfd8f7de650b2a6bb9c53ed32449ee48df8
simion samuel <samuel.simion@student.moringaschool.com>
2025-03-21 21:36:47 +0300

    assig

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const btn = document.getElementById('button');
 function addingEventListener() {
+    btn.addEventListener('click', () => alert('i\'ve been clicked'));
+
 }
+addingEventListener();
+
+/*describe("index.js", () => {
+  let input;
+
+  beforeEach(function() {
+    input = document.getElementById('button');
+    sinon.spy(input, 'addEventListener');
+  })
+
+  it("binds an event listener in addingEventListener()", () => {
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+  })
+})
+ */
